aefc441b69cc293c2e6014a8e806a62d73173dc7
Al3nMicL <https://twitter.com/Al3nMicL>
2019-04-08 21:01:35 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+// Task 1
+function paragraphSelector() {
+  return $('p');
+}
+
+// Task 2
+function lastImageSelector() {
+  return $('img:last');
+}
+
+// Task 3
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+// Task 4
+function divSelector() {
+  return $('.pics');
+}
+
+// Task 5
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
